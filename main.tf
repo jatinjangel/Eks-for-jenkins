205545059a74d6a132d4a467ee2145851619c7ff
code with J3 <jatinjangel27@gmail.com>
2026-03-19 18:43:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-##############################
-# EKS Cluster
-##############################
 resource "aws_eks_cluster" "aws_cluster" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -8,28 +5,40 @@ resource "aws_eks_cluster" "aws_cluster" {
   vpc_config {
     subnet_ids = var.subnet_ids
 
-    endpoint_public_access  = true   
+     endpoint_public_access  = true   
     endpoint_private_access = true 
     public_access_cidrs     = ["0.0.0.0/0"] 
+    
+    
   }
   
   access_config {
-    authentication_mode                         = "API"
+    authentication_mode = "API"
     bootstrap_cluster_creator_admin_permissions = true
   }
 
-  depends_on = [
-    aws_iam_role.eks_cluster_role
-  ]
+  depends_on = [aws_iam_role.eks_cluster_role] 
 
   timeouts {
-    create = "15m"
+ create = "15m"
   }
+  
 }
 
-##############################
-# Node Group (FIRST)
-##############################
+resource "aws_eks_addon" "eks_addon" {
+
+  cluster_name = aws_eks_cluster.aws_cluster.name
+  for_each = toset(var.cluster_addons)
+  addon_name = each.value
+  resolve_conflicts_on_update = "PRESERVE"
+  timeouts {
+    create = "10m"
+  }
+
+  depends_on = [aws_eks_node_group.node_group]
+  
+}
+
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = aws_eks_cluster.aws_cluster.name
   node_group_name = "${var.cluster_name}-node-group"
@@ -41,39 +50,19 @@ resource "aws_eks_node_group" "node_group" {
     max_size     = 3
     min_size     = 1
   }
-
   instance_types = var.ng_instance_types
-  disk_size      = 20
-  ami_type       = "AL2_x86_64"   # ✅ FIXED
-  capacity_type  = "ON_DEMAND"
+  disk_size = 20
+  ami_type = "AL2023_x86_64_STANDARD"
+  capacity_type = "ON_DEMAND"
 
-  depends_on = [
-    aws_eks_cluster.aws_cluster,
-    aws_iam_role.nodegroup_role,
-    aws_iam_role_policy_attachment.nodegroup_attachment[*]
-  ]
+  depends_on = [aws_eks_cluster.aws_cluster, aws_iam_role.nodegroup_role, aws_eks_addon.eks_addon, aws_iam_role_policy_attachment.nodegroup_attachment]
 
   timeouts {
+
     create = "15m"
+    
   }
-}
+ 
 
-##############################
-# Addons (AFTER Node Group)
-##############################
-resource "aws_eks_addon" "eks_addon" {
-
-  cluster_name = aws_eks_cluster.aws_cluster.name
-  for_each     = toset(var.cluster_addons)
-  addon_name   = each.value
-
-  resolve_conflicts_on_update = "PRESERVE"
-
-  timeouts {
-    create = "10m"
-  }
-
-  depends_on = [
-    aws_eks_node_group.node_group   # ✅ correct flow
-  ]
-}
+  
+}   
