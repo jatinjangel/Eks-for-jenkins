25a40c7bc85e224c95fbd51a35e8300468c38be1
code with J3 <jatinjangel27@gmail.com>
2026-03-20 17:46:26 +0530

    Refactor EKS cluster and node group configuration

--- main.tf
+++ main.tf
@@ -1,68 +1,37 @@
-resource "aws_eks_cluster" "aws_cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = var.subnet_ids
-
-     endpoint_public_access  = true   
-    endpoint_private_access = true 
-    public_access_cidrs     = ["0.0.0.0/0"] 
-    
-    
-  }
-  
-  access_config {
-    authentication_mode = "API"
-    bootstrap_cluster_creator_admin_permissions = true
-  }
-
-  depends_on = [aws_iam_role.eks_cluster_role] 
-
-  timeouts {
- create = "15m"
-  }
-  
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_eks_addon" "eks_addon" {
-
-  cluster_name = aws_eks_cluster.aws_cluster.name
-  for_each = toset(var.cluster_addons)
-  addon_name = each.value
-  resolve_conflicts_on_update = "PRESERVE"
-  timeouts {
-    create = "10m"
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
-
-  depends_on = [aws_eks_cluster.aws_cluster, aws_iam_role.eks_cluster_role,aws_iam_role_policy_attachment.eks_cluster_role_attachment]
-  
 }
 
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.aws_cluster.name
-  node_group_name = "${var.cluster_name}-node-group"
-  node_role_arn   = aws_iam_role.nodegroup_role.arn
-  subnet_ids      = var.subnet_ids
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.0.0"
 
-  scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 1
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
+
+  cluster_endpoint_public_access  = true
+  cluster_endpoint_private_access = true
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = [var.node_instance_type]
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 2
+    }
   }
-  instance_types = var.ng_instance_types
-  disk_size = 20
-  ami_type = "AL2023_x86_64_STANDARD"
-  capacity_type = "ON_DEMAND"
 
-  depends_on = [aws_eks_cluster.aws_cluster, aws_iam_role.nodegroup_role, aws_eks_addon.eks_addon, aws_iam_role_policy_attachment.nodegroup_attachment]
-
-  timeouts {
-
-    create = "15m"
-    
+  tags = {
+    Environment = "dev"
   }
- 
-
-  
 }
