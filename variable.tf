a26c9cc7e6cc6ebb199eb4a86b36409e7786bd38
code with J3 <jatinjangel27@gmail.com>
2026-03-20 17:48:33 +0530

    Refactor variable.tf to streamline variables
    Removed unused variables and added default values for region, cluster_name, and node_instance_type.

--- variable.tf
+++ variable.tf
@@ -1,32 +1,11 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
 variable "cluster_name" {
-  description = "The name of the EKS cluster."
-  type        = string
-  
-}
-variable "subnet_ids" {
-  description = "A list of subnet IDs for the EKS cluster."
-  type        = list(string)
-  
+  default = "cluster-01"
 }
 
-variable "cluster_addons" {
-  description = "A list of EKS cluster addons to be installed."
-  type        = list(string)
-  
-}
-variable "eks_node_group_policies" {
-  description = "List of IAM policy ARNs to attach to the node group role"
-  type        = list(string)
-  default = [
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-  ]
-}
-
-variable "ng_instance_types" {
-  description = "The instance types for the EKS node group."
-  type        = list(string)
-  default = [ "t3.small" ]
+variable "node_instance_type" {
+  default = "t3.small"
 }
